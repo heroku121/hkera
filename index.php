96737ade3ddb10ce416f9d150f7d28940917fe77
heroku121 <130366513+heroku121@users.noreply.github.com>
2023-05-09 16:02:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kerassentials | Review</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0;">
+    <META NAME="robots" CONTENT="noindex,nofollow">
+    <link rel="icon" type="image/x-icon" href="./img/favicon.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <script type="text/javascript" src="https://bestofferhere.link/kerassentials"></script>
+</head>
+
+<body>
+    <div class="hdr-kdfdd1t-od">
+        <div class="bx-kdfdd1t-od">
+            <div class="ste-inf-kdfdd1t-od fd-kdfdd1t-od js-1 a-cntr">
+                <div class="bd-logo-kdfdd1t-od">
+                    <a href="https://bestofferhere.link/kerassentials">
+                        <h1>Kerassentials</h1>
+                    </a>
+                </div>
+                <div class="hd-btn-kdfdd1t-od d-nn">
+                    <a href="https://bestofferhere.link/kerassentials"
+                        class="ct-odr">ORDER NOW</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------------->
+    <div class="mn-sec-kdfdd1t-od">
+        <div class="bx-kdfdd1t-od">
+            <div class="man-sec-sd-kdfdd1t-od fd-kdfdd1t-od js-1 a-cntr kdfdd1t-gpd">
+                <div class="manp-sec-img t-ctr ">
+                    <img src="./img/manbnner.png" class="rsp1">
+                </div>
+                <div class="man-rt-cont mmbt" style="padding:15px;">
+                    <div style="border:2px solid #fff; padding:10px;">
+                        <p class="kdfdd1t-od-ssl">TRY KERASSENTIAL TODAY</p>
+                        <h1 style="color:#fff;margin:0px;">Fight Fungus And Support Healthy Nails And Skin</h1>
+                        <p style="color:#fff">This formula is unlike anything you\'ve ever tried or experienced in your life before. You will be able to support a fungus-free life, without worrying about itching or foul smell. All that while enjoying flawless nails and skin.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-------->
+    <div class="st-ps-ptd mbt">
+        <div class="bx-kdfdd1t-od">
+            <div class="rsh-strt-ptd fd-kdfdd1t-od js-1 gpkdfdd1t-od">
+                <div class="ft-kdfdd1t-ods mmbt2">
+                    <p><b>Healthy Nails and Better Skin</b></p>
+                </div>
+                <div class="ft-kdfdd1t-ods mmbt2">
+                    <p><b>Improved Elastic and Hydrated Skin</b></p>
+                </div>
+                <div class="ft-kdfdd1t-ods mmbt2">
+                    <p><b>100% Natural With No Side Effects</b></p>
+                </div>
+                <div class="ft-kdfdd1t-ods mmbt2">
+                    <p><b>Kerassentials Reduced Inflammation</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!------------------>
+    <div class="wt">
+        <div class="bx-kdfdd1t-od">
+            <div class="wt-ct-ptd t-ctr">
+                <h1 style="color:#0a7e1c">What Is Kerassentials?</h1>
+
+                <p><b>"The supplement uses 100% all natural ingredients to heal, rebuild & rejuvinate skin & nails."</b></p>
+
+                <p>Kerassentials is an advanced blend of essential oils that help in restoring the skin and nail wellbeing.</p>
+
+                <p>This superior skin-supporting formula helps in preventing different types of nail fungus and painful infections and it ensures to keep your skin and nails healthy and shiny.</p>
+
+                <p>With the regular use of the formula, one can achieve healthy skin and it offers sufficient nourishment for super healthy hair and nails while preventing fungus infections.</p>
+                <div class="fd-kdfdd1t-od js-1" style="gap:20px;">
+                    <div class="abt-img">
+                        <img src="./img/ab1.png" class="ab-rsp">
+                    </div>
+                    <div class="abt-img">
+                        <img src="./img/ab3.png" class="ab-rsp">
+                    </div>
+                    <div class="abt-img">
+                        <img src="./img/ab2.png" class="ab-rsp">
+                    </div>
+                </div>
+                <div class="mbs-cntr" style="margin:30px 0px;">
+                    <a href="https://bestofferhere.link/kerassentials"
+                        class="ct-odr">Hurry Limited Time Offer !!</a>
+                </div>
+
+                <h1 style="color:#008000">June 2022 - New Scientific Discovery</h1>
+                <p>If you have been struggling for months or years with toenail itching and foul smell… Trying diets and exercise but with no real results… You should know that it’s not your fault.</p>
+                <p>According to scientists from the the U.S. Centers for Disease Control and Prevention, decades of people using treatments but stopping short of completely killing the fungus meant the remaining fungus learnt how to survive.</p>
+
+                <p>That made the treatment useless the next time around as the fungus mutated into a stronger kind of fungus.</p>
+
+                <p>The meds you took by now, just like antibiotics, ended up creating stronger, mutated fungi. This led to antifungal resistance, which makes it almost impossible to get rid of fungus.</p>
+
+                <h1 style="color:#008000; margin-top:30px;">Ingredients of Kerassentials</h1>
+                <p><b>“A bespoke proprietary formula of 4 special high-quality oils, along with a powerful mix of 9 oils and minerals.”</b></p>
+            </div>
+            <div class="rsh-strt-ptd fd-kdfdd1t-od js-1 gpkdfdd1t-od" style="margin-top:40px;">
+                <div class="rsh-rght-kdfdd1t-od mtct">
+                    <img src="./img/scientistsBg-mobile.webp" class="resp">
+                    <div class="kdfdd1t-igt">
+                        <h3>Kerassentials is a Pure Formula. Create Quick Order Today From Web Site. Buy Kerassentials. Profit From Special Monthly Discount on All Available Options.</h3>
+                    </div>
+                </div>
+                <div class="rsh-ct-ptkdfdd1t-od t-wht mgred">
+                    <div class="fd-kdfdd1t-od js-1 kdfdd1t-gpd">
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing1.png" class="itg-rsp">
+                            <h3 style="margin:0px;">LAVENDER OIL</h3>
+                            <p>Protects nail keratin. Supports the nails and skin. Fights against strong fungus.</p>
+                        </div>
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing2.png" class="itg-rsp">
+                            <h3 style="margin:0px;">ORGANIC FLAXSEED OIL</h3>
+                            <p>Boosts skin’s natural. Helps with inflammation. Superfood for your skin.</p>
+                        </div>
+                    </div>
+                    <div class="fd-kdfdd1t-od js-1 kdfdd1t-gpd">
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing3.png" class="itg-rsp">
+                            <h3 style="margin:0px;">ALMOND OIL</h3>
+                            <p>Helps prevent fungus. Protects against infections. Supports healthy nails</p>
+                        </div>
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing4.png" class="itg-rsp">
+                            <h3 style="margin:0px;">TEA TREE OIL</h3>
+                            <p>Strong antifungal properties. Helps curb fungus growth. Safe and effective</p>
+                        </div>
+                    </div>
+                    <div class="fd-kdfdd1t-od js-1 kdfdd1t-gpd">
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing5.png" class="itg-rsp">
+                            <h3 style="margin:0px;">LEMONGRASS OIL</h3>
+                            <p>Efficient antifungal. Prevents future infection. Helps with inflammation</p>
+                        </div>
+                        <div class="kdfdd1t-od-ft">
+                            <img src="./img/ing6.png" class="itg-rsp">
+                            <h3 style="margin:0px;">ALOE VERA</h3>
+                            <p>Soothes the skin. Strong antifungal. Moisturizes the skin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------------------->
+
+    <!------------->
+    <div class="rsh mbt">
+        <div class="bx-kdfdd1t-od">
+            <div class="rsh-strt-ptd">
+                <h2 style="text-align:center">Real Kerassentials Users. Real Life-Changing Results</h2>
+                <img src="./img/review-images2-new.png" class="rvw-rsp">
+                <div class="fd-kdfdd1t-od js-1 a-cntr " style="margin-top:30px; gap:20px;">
+                    <div class="revw-bx">
+                        <h3>Brandon Johnson is happy with his new nails…</h3>
+                        <small>Verified Purchase</small>
+                        <p>“I’ve tried many products to get rid of foot fungus before Kerassentials. I even had one nail removed but when it grew back, the fungus returned. Kerassentials is the only thing that actually helped.”</p>
+                        <p>Brandon Johnson - Chicago, USA</p>
+                    </div>
+                    <div class="revw-bx">
+                        <h3>Riley Harrison has shared the product with her entire family…</h3>
+                        <small>Verified Purchase</small>
+                        <p>“I’ve been struggling with foot fungus since high school and this is the only thing that helped. I still can’t believe how great it worked!”</p>
+                        <p>Riley Harrison - Wyoming, USA</p>
+                    </div>
+                </div>
+                <div class="mbs-cntr" style="margin-top:30px; text-align:center;">
+                    <a href="https://bestofferhere.link/kerassentials"
+                        class="ct-odr">Hurry Limited Time Offer !!</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-------------------->
+    <div class="itg">
+        <div class="bx-kdfdd1t-od">
+            <div class="itg-start-ptd">
+                <h1 class="t-ctr" style="color:#0a7e1c;">Frequently Asked Questions</h1>
+
+                <div class="itg-ct-ptd">
+                    <div class="itg-box-ptd" style="background-color:#d5f8d7">
+                        <h3>1. So what is Kerassentials, and how does it work again?</h3>
+                        <p>Kerassentials is an extremely potent combination of oils and skin supporting vitamins that you can apply every day after you shower to give your nail bed the boost it needs. These carefully selected ingredients will give your skin
+                            the support it needs to constantly rebuild itself and stay healthy. Kerassentials is the only complete formula that maintains the health of your nails in multiple ways.</p>
+                    </div>
+                    <div class="itg-box-ptd" style="background-color:#c0faba">
+                        <h3>2. When can I see results?</h3>
+                        <p>They know you are excited to finally rid this annoying fungus, so they can tell that you start noticing improvements immediately. Because your toenails will look more alive. The itchiness will be soothed. And within the first weeks
+                            you should be noticing new healthy pink nails growing out to replace the damaged areas. But They can tell you the biggest change will be in your confidence seeing yourself finally beat this thing that has been haunting you
+                            for so long.</p>
+
+                    </div>
+                    <div class="itg-box-ptd" style="background-color:#88d77f">
+                        <h3>3. How does the guarantee work again?</h3>
+                        <p>When you click on any of the packages below and place your order, you will be covered by our full 60 day, 100% money back guarantee. This means if you change your mind at any time, or for any reason at all, you can just email our
+                            customer support team and we will refund your entire investment in Kerassentials no questions asked. There is absolutely no risk for you.</p>
+                    </div>
+                </div>
+                <div class="itg-ct-ptd">
+                    <div class="itg-box-ptd" style="background-color:#c0faba">
+                        <h3>4. Ok I am ready to make my order. What now?</h3>
+                        <p>Just click on any of the packages you see below this video and place your secure order. Make sure you do it before stock runs out. The 6 bottle package is by far the most popular option because it guarantees you will not run out
+                            of Kerassentials during the course of your treatment.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------------------->
+
+
+    <!-------------------->
+    <div class="my-bc">
+        <div class="bx-kdfdd1t-od">
+            <h1 class="t-ctr" style="color:#2f6f24">Claim Your Discounted Kerassentials Below While Stocks Last!</h1>
+            <h3 class="t-ctr mbt">Secure Your Reserved Bottles While Stocks Last</h3>
+            <div class="p-box fd-kdfdd1t-od js-1 a-cntr ">
+                <div class="p-bx-kdfdd1t-od-c t-ctr">
+                    <a href="https://bestofferhere.link/kerassentials"><img src="./img/price1.png" class="resp-p"></a>
+                </div>
+                <div class="p-bx-kdfdd1t-od-c t-ctr">
+                    <a href="https://bestofferhere.link/kerassentials"><img src="./img/price3.png" class="resp-p"></a>
+                </div>
+                <div class="p-bx-kdfdd1t-od-c t-ctr">
+                    <a href="https://bestofferhere.link/kerassentials"><img src="./img/price2.png" class="resp-p"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------------------->
+    <div class="mn-g">
+        <div class="bx-kdfdd1t-od">
+            <div class="mn-back-ptd fd-kdfdd1t-od js-1 a-cntr">
+                <div class="mn-img t-ctr" style="background-color:#057805;border-radius:100%">
+                    <a href="https://bestofferhere.link/kerassentials"><img src="./img/guarantee_badge.png" class="resp"></a>
+                </div>
+                <div class="mn-ct">
+                    <h2>100% Satisfaction 60-Day Money Back Guarantee</h2>
+                    <p>Your order today is covered by their iron-clad 60-day 100% money-back guarantee. If you are not impressed with the results, then just write to them and they’ll refund every single cent.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!------------------->
+    <div class="f-toor">
+        <div class="bx-kdfdd1t-od">
+            <div class="ct-fot t-ctr">
+                <p><a href="">Privacy Policy</a> | <a href="">Terms</a> | <a href="">Contact Us</a></p>
+                <small>Copyright © 2023 Kerassentialsonline.store | All Rights Reserved</small><br>
+                <small>Please note that the information They provide is not intended to replace consultation with a qualified professional. They encourage you to inform your physician of changes you make to your lifestyle and discuss these with him or her. For questions or concerns about any conditions you may have, please contact your doctor.
+            <br>
+			Statements on this website have not been evaluated by the Food and Drug Administration. Products are not intended to diagnose, treat, cure or prevent any disease. If you are pregnant, nursing, or have a condition, consult your physician before using their products.
+            </small>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+';
+?>
